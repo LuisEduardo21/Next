85b5ab903d7603bb36b2af02b0c267c6cff4510d
luis <lfdasilva9@gmail.com>
2020-10-17 18:21:48 -0300

    Configurando o Typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+
+type Props = {
+  title: String
+}
+
+export default function Home({ title = "React Avançado" }: Props) {
+  return (
+    <div className="container">
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="main">
+        <h1 className="title">
+         {title}
+        </h1>
+
+        <p className={"description"}>
+          Get started by editing{' '}
+          <code className={"code"}>pages/index.js</code>
+        </p>
+      </main>
+    </div>
+  )
+}
